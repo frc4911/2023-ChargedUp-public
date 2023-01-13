8d7f2ddb74d19a3693743003a64602e3925f5aac
SuperDuperGreen <118789120+SuperDuperGreen@users.noreply.github.com>
2023-01-12 20:22:53 -0800

    Created stub for claw

---
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+/**
+ * Subsystem for controlling the 
+ */
+public class ClawSubsystem implements Subsystem{
+    
+}
