8909500be6d0dbb671879c2ccea5d717ba047453
Riley Brewer <riley.brewer@gmail.com>
2023-01-14 10:31:26 -0800

    Fix comments and rename subsystem.

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+/**
+ * Subsystem for handling vision.
+ */
+public class VisionSubsystem implements Subsystem {
+    
+}
