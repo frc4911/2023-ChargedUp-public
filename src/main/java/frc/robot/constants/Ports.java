f64e39cce589c7c4bab779a5139d252b84f629a6
SuperDuperGreen <118789120+SuperDuperGreen@users.noreply.github.com>
2023-01-14 13:18:01 -0800

    Added CLIMB_SOLENOID_PORT

--- src/main/java/frc/robot/constants/Ports.java
+++ src/main/java/frc/robot/constants/Ports.java
@@ -25,6 +25,8 @@ public class Ports {
     public static final int BACK_LEFT_CANCODER   = 2;
     public static final int BACK_RIGHT_CANCODER  = 3;
 
+    public static final int CLIMB_SOLENOID_PORT = 1;
+
     public static final int PIGEON = 0;
     public static final int PRESSURE_SENSOR = 2; //Analog Input
 }
