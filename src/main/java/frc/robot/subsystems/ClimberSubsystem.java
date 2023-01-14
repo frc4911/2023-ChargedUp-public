c3139fa94b02ec52644e02a7d1fdd9d4852bce43
SuperDuperGreen <118789120+SuperDuperGreen@users.noreply.github.com>
2023-01-14 13:21:26 -0800

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,28 +1,25 @@
 package frc.robot.subsystems;
 
-import java.time.Period;
-
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.Constants;
+import frc.robot.constants.Ports;
 
-/**
- * Subsystem for controlling climbing
+/*
+ Subsystem for controlling climbing
  */
 public final class ClimberSubsystem implements Subsystem {
-    private final Solenoid climbSolenoid = new Solenoid(PneumaticsModuleType.REVPH, Constants.CLIMB_SOLANOID_PORT);
+    private final Solenoid mClimbSolenoid = new Solenoid(PneumaticsModuleType.REVPH, Ports.CLIMB_SOLENOID_PORT);
 
     private boolean extended = false;
 
     public void setExtended (boolean extended) {
 
-        this.extended = extended;
+    mClimbSolenoid.set(extended);
 
     }
     @Override
     public void periodic () {
-        climbSolenoid.set(extended);
+//heh
     }
 }
-
