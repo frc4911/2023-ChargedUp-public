9693e7d8b27385218c13d227eda2a07639e7cf80
SuperDuperGreen <118789120+SuperDuperGreen@users.noreply.github.com>
2023-01-14 13:17:23 -0800

    Added solenoids and put it in ports

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,10 +1,28 @@
 package frc.robot.subsystems;
 
+import java.time.Period;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants;
 
 /**
- * Subsystem for controlling climbing.
+ * Subsystem for controlling climbing
  */
 public final class ClimberSubsystem implements Subsystem {
-    
+    private final Solenoid climbSolenoid = new Solenoid(PneumaticsModuleType.REVPH, Constants.CLIMB_SOLANOID_PORT);
+
+    private boolean extended = false;
+
+    public void setExtended (boolean extended) {
+
+        this.extended = extended;
+
+    }
+    @Override
+    public void periodic () {
+        climbSolenoid.set(extended);
+    }
 }
+
