bf46a60aa9c2a3b8d09557dec89e36ff45274434
SuperCoderTrek <118789031+SuperCoderTrek@users.noreply.github.com>
2023-01-28 14:22:54 -0800

    Claw -> Slurp
    Changed Claw Subsystem to Slurpp Subsystem and changed open/close claw to Slurpp and Spit respectively

--- src/main/java/com/cyberknights4911/robot/subsystems/SlurppSubsystem.java
+++ src/main/java/com/cyberknights4911/robot/subsystems/SlurppSubsystem.java
@@ -3,14 +3,16 @@ package com.cyberknights4911.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
- * Subsystem for controlling the Claw.
+ * Subsystem for controlling the Slurpp.
  */
-public final class ClawSubsystem implements Subsystem {
-    public void openClaw() {
+public final class SlurppSubsystem implements Subsystem {
+    public SlurppSubsystem(){}
+    public void slurpp() {
         // TODO make it work
     }
-    public void closeClaw() {
+    public void spit() {
         // TODO make it also work
 
     }
 }
+
