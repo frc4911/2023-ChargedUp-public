bf203424d3fb519b5e5558c4fbd4e60be408c2f7
Riley Brewer <riley.brewer@gmail.com>
2023-01-30 17:17:39 -0800

    Add some comments for clarity

--- src/main/java/com/cyberknights4911/robot/subsystems/SlurppSubsystem.java
+++ src/main/java/com/cyberknights4911/robot/subsystems/SlurppSubsystem.java
@@ -3,16 +3,21 @@ package com.cyberknights4911.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
- * Subsystem for controlling the Slurpp.
+ * Subsystem for controlling the Slurpp, which is responsible for taking in and releasing game
+ * pieces.
  */
 public final class SlurppSubsystem implements Subsystem {
-    public SlurppSubsystem(){}
+
+    public SlurppSubsystem() {}
+
+    /** "Slurpp" up a game piece. */
     public void slurpp() {
         // TODO make it work
     }
+
+    /** "Spit" out a game piece. */
     public void spit() {
         // TODO make it also work
 
     }
 }
-
