a814a2155e6d38a6ea2307dfc4eabe29d68e6346
Riley Brewer <117626660+Krylez@users.noreply.github.com>
2023-02-01 19:21:32 -0800

    Cleanup: remove unused constants and annotations (#87)

--- src/main/java/com/cyberknights4911/robot/constants/Constants.java
+++ src/main/java/com/cyberknights4911/robot/constants/Constants.java
@@ -2,12 +2,6 @@ package com.cyberknights4911.robot.constants;
 
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.math.geometry.Pose2d;
-//import frc.robot.limelight.CameraResolution;
-//import frc.robot.limelight.PipelineConfiguration;
-//import libraries.cheesylib.geometry.Pose2d;
-//import libraries.cheesylib.geometry.Rotation2d;
-//import libraries.cheesylib.geometry.Translation2d;
-import libraries.cheesylib.vision.GoalTrackerConfig;
 
 public class Constants {
     /* All distance measurements are in inches, unless otherwise noted. */
@@ -18,15 +12,6 @@ public class Constants {
 
     public static final String CANIVORE_NAME = "CANivore";
 
-    public static final double LOOPER_DT = 0.02;
-
-    public static final double EPSILON = 0.0001;
-
-    public static final boolean IS_USING_COMPBOT = true;
-    public static final boolean IS_USING_TRACTION_WHEELS = true;
-
-    public static final boolean DEBUGGING_OUTPUT = true;
-
     //2022 Physical Robot Dimensions (including bumpers)
     public static final double ROBOT_WIDTH = 33;
     public static final double ROBOT_LENGTH = 34.5;
@@ -34,9 +19,6 @@ public class Constants {
     public static final double ROBOT_LENGTH_METERS = 0.8763;
     public static final double ROBOT_HALF_WIDTH = ROBOT_WIDTH / 2.0;
     public static final double ROBOT_HALF_LENGTH = ROBOT_LENGTH / 2.0;
-    public static final double ROBOT_PROBE_EXTRUSION = 4.0;
-
-    public static final double BALL_RADIUS = 6.5;
 
     public static final double ROBOT_WIDTH_METERS_2023 = 0.8763;
     public static final double ROBOT_LENGTH_METERS_2023 = 0.8763;
@@ -77,28 +59,7 @@ public class Constants {
 
     // END NEW SWERVE
 
-    // LIMELIGHT
-    // TODO:  Create Robot specific configurations
-    // Goal Tracker
-    public static final boolean USE_TOP_CORNERS = true;
-
-    public static final double IMAGE_CAPUTRE_LATENCY = 11.0 / 1000.0; // seconds
-
-    public static final double MAX_TRACKER_DISTANCE = Units.feetToMeters(16.0);
-    public static final double MAX_GOAL_TRACK_AGE = 1.0; //2.5;
-    public static final double MAX_GOAL_TRACK_SMOOTHING_TIME = 0.5;
-    public static final double CAMERA_FRAME_RATE = 90.0; // fps
-
-    public static GoalTrackerConfig GOAL_TRACKER_CONFIG = new GoalTrackerConfig(
-            MAX_TRACKER_DISTANCE, MAX_GOAL_TRACK_AGE, MAX_GOAL_TRACK_SMOOTHING_TIME, CAMERA_FRAME_RATE);
-
-    public static final double TRACK_STABILITY_WEIGHT = 0.0;
-    public static final double TRACK_AGE_WEIGHT = 10.0;
-    public static final double TRACK_SWITCHING_WEIGHT = 100.0;
-
     // LED/Canifier
-    public static final int CANIFER_ID = 0;
-    public static final int CAN_TIMEOUTS_MS = 10; // use for on the fly updates
     public static final int LONG_CAN_TIMEOUTS_MS = 100; // use for constructors
 
     // Controller port
