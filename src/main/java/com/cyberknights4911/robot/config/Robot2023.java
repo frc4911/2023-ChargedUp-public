4ca5f4ef7e2b99e02d9536b3f6c16539c75225ae
Caleb West <calebewest@outlook.com>
2023-03-21 18:52:25 -0700

    Decrease speed

--- src/main/java/com/cyberknights4911/robot/config/Robot2023.java
+++ src/main/java/com/cyberknights4911/robot/config/Robot2023.java
@@ -36,7 +36,7 @@ public class Robot2023 implements RobotConfiguration {
         new DoublePreference("HEADING_MOTOR_F", 0.0);
         
     private static final DoublePreference MAX_SPEED_METERS_PER_SECOND = 
-        new DoublePreference("MAX_SPEED_METERS_PER_SECOND", Units.feetToMeters(5));
+        new DoublePreference("MAX_SPEED_METERS_PER_SECOND", 3);//Units.feetToMeters(5));
         private static final DoublePreference MAX_SPEED_RADIANS_PER_SECOND = 
             new DoublePreference("MAX_SPEED_RADIANS_PER_SECOND", Math.toRadians(100));
 
