51a0139c0ee5b7fb794a6fe5b97f34f66f413678
Riley Brewer <riley.brewer@gmail.com>
2023-04-03 23:00:59 -0700

    Update port constants

--- src/main/java/com/cyberknights4911/robot/subsystems/drive/v2/SwerveModuleConstants.java
+++ src/main/java/com/cyberknights4911/robot/subsystems/drive/v2/SwerveModuleConstants.java
@@ -8,42 +8,42 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public final class SwerveModuleConstants {
     private static final CotsFalconSwerveConstants PHYSICAL_SWERVE_MODULE =
         CotsFalconSwerveConstants.SDSMK4i(
-            CotsFalconSwerveConstants.DriveGearRatios.SDSMK4i_L2
+            CotsFalconSwerveConstants.DriveGearRatios.SDSMK4i_L1
         );
 
     public static final SwerveModuleConstants FRONT_LEFT = 
         new SwerveModuleConstants(
             0,
-            Ports.ROBOT_2022_FRONT_LEFT_DRIVE,
-            Ports.ROBOT_2022_FRONT_LEFT_STEER,
-            Ports.ROBOT_2022_FRONT_LEFT_CANCODER,
+            Ports.Drive.FRONT_LEFT_DRIVE,
+            Ports.Drive.FRONT_LEFT_STEER,
+            Ports.Drive.FRONT_LEFT_CANCODER,
             Rotation2d.fromDegrees(150.38),
             PHYSICAL_SWERVE_MODULE);
 
     public static final SwerveModuleConstants FRONT_RIGHT = 
         new SwerveModuleConstants(
             1,
-            Ports.ROBOT_2022_FRONT_RIGHT_DRIVE,
-            Ports.ROBOT_2022_FRONT_RIGHT_STEER,
-            Ports.ROBOT_2022_FRONT_RIGHT_CANCODER,
+            Ports.Drive.FRONT_RIGHT_DRIVE,
+            Ports.Drive.FRONT_RIGHT_STEER,
+            Ports.Drive.FRONT_RIGHT_CANCODER,
             Rotation2d.fromDegrees(2.29),
             PHYSICAL_SWERVE_MODULE);
 
     public static final SwerveModuleConstants BACK_LEFT = 
         new SwerveModuleConstants(
             2,
-            Ports.ROBOT_2022_BACK_LEFT_DRIVE,
-            Ports.ROBOT_2022_BACK_LEFT_STEER,
-            Ports.ROBOT_2022_BACK_LEFT_CANCODER,
+            Ports.Drive.BACK_LEFT_DRIVE,
+            Ports.Drive.BACK_LEFT_STEER,
+            Ports.Drive.BACK_LEFT_CANCODER,
             Rotation2d.fromDegrees(83.23),
             PHYSICAL_SWERVE_MODULE);
 
     public static final SwerveModuleConstants BACK_RIGHT = 
         new SwerveModuleConstants(
             3,
-            Ports.ROBOT_2022_BACK_RIGHT_DRIVE,
-            Ports.ROBOT_2022_BACK_RIGHT_STEER,
-            Ports.ROBOT_2022_BACK_RIGHT_CANCODER,
+            Ports.Drive.BACK_RIGHT_DRIVE,
+            Ports.Drive.BACK_RIGHT_STEER,
+            Ports.Drive.BACK_RIGHT_CANCODER,
             Rotation2d.fromDegrees(244.07),
             PHYSICAL_SWERVE_MODULE);
 
